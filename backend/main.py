a6b907bbd0aa0224a8c020f239dd3aa6d4e5bf05
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-16 14:34:46 -0300

    added imports and path /health

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-  return {"message": "World"}
+origins = [ "http://localhost:5173", "http://localhost:5174/"]
+
+app.add_middleware( CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
+
+@app.get("/health")
+async def check_health():
+  return {"message": "healthy"}
 
